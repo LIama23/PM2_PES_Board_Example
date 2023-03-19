4d5c962cc6c0cc9fd14c3a5567ce1910b4d485d3
Liama23 <daniel.stockmann@hotmail.com>
2023-03-19 21:32:11 +0100

    Empty project

--- main.cpp
+++ main.cpp
@@ -2,59 +2,8 @@
 
 #include "PM2_Drivers.h"
 
-# define M_PI 3.14159265358979323846 // number pi, an example in case you need it
-
-
-bool do_execute_main_task = false; // this variable will be toggled via the user button (blue button) and decides whether to execute the main task or not
-bool do_reset_all_once = false;    // this variable is used to reset certain variables and objects and shows how you can run a code segment only once
-
-// objects for user button (blue button) handling on nucleo board
-DebounceIn user_button(PC_13);  // create InterruptIn interface object to evaluate user button falling and rising edge (no blocking code in ISR)
-void user_button_pressed_fcn(); // custom functions which get executed when user button gets pressed, definition below
-
-
 // main runs as an own thread
 int main()
 {
     
-    // attach button fall function to user button object, button has a pull-up resistor
-    user_button.fall(&user_button_pressed_fcn);
-
-    // while loop gets executed every main_task_period_ms milliseconds (simple aproach to repeatedly execute main)
-    const int main_task_period_ms = 50; // define main task period time in ms e.g. 50 ms -> main task runs 20 times per second
-    Timer main_task_timer;              // create Timer object which we use to run the main task every main_task_period_ms
-
-
-    // led on nucleo board
-    DigitalOut user_led(LED1);       // create DigitalOut object to command user led
-
-
-    main_task_timer.start();
-    
-    // this loop will run forever
-    while (true) {
-
-        main_task_timer.reset();
-
-        if (do_execute_main_task) {
-          
-            // toggling the user led
-            user_led = !user_led;
-
-        }
-
-        // do only output via serial what's really necessary, this makes your code slow
-        printf("hello im a float %3f\r\n", 0.777f);
-
-        // read timer and make the main thread sleep for the remaining time span (non blocking)
-        int main_task_elapsed_time_ms = std::chrono::duration_cast<std::chrono::milliseconds>(main_task_timer.elapsed_time()).count();
-        thread_sleep_for(main_task_period_ms - main_task_elapsed_time_ms);
-    }
-}
-
-void user_button_pressed_fcn()
-{
-    // do_execute_main_task if the button was pressed
-    do_execute_main_task = !do_execute_main_task;
-    if (do_execute_main_task) do_reset_all_once = true;
 }
